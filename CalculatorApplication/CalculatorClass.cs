2d8c4a4ab9847c0bd95e679fdcdd3f35cae36f02
Windows 10 PRO <Windows 10 PRO@DESKTOP-32E3M3E>
2024-09-08 17:45:28 +0800

    Add project files.

---
+++ CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+
+    public delegate T Formula <T>(T txtBoxInput1, T txtBoxInput2);
+
+    internal class CalculatorClass
+    {
+        public Formula <double> info;
+
+        public double GetSum(double txtBoxInput1, double txtBoxInput2)
+        {
+            return txtBoxInput1  + txtBoxInput2;
+        }
+
+        public double GetDifference(double txtBoxInput1, double txtBoxInput2)
+        {
+            return txtBoxInput1 - txtBoxInput2;
+        }
+
+        public double GetProduct(double txtBoxInput1, double txtBoxInput2)
+        {
+            return txtBoxInput1 * txtBoxInput2;
+        }
+
+        public double GetQuotient(double txtBoxInput1, double txtBoxInput2)
+        {
+            return txtBoxInput1 / txtBoxInput2;
+        }
+
+        public event Formula <double> CalculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Added the Delegate");
+            }
+            remove
+            {
+                Console.WriteLine("Removed the Delegate");
+            }
+        }
+    }
+}
---
+++ CalculatorApplication/FrmCalculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+    public partial class FrmCalculator : Form
+    {
+        CalculatorClass cal;
+
+        double num1;
+        double num2;
+        
+
+
+        public FrmCalculator()
+        {
+            InitializeComponent();
+
+            cal = new CalculatorClass();
+
+            cbOperator.Items.Add("+");
+            cbOperator.Items.Add("-");
+            cbOperator?.Items.Add("*");
+            cbOperator?.Items.Add("/");
+
+            
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(txtBoxInput1.Text);
+            num2 = Convert.ToDouble(txtBoxInput2.Text);
+
+            switch (cbOperator.SelectedItem.ToString())
+            {
+                case "+":
+                    cal.CalculateEvent += new Formula <double> (cal.GetSum);
+                    lblDisplayTotal.Text = cal.GetSum(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula <double>(cal.GetSum);
+                    break;
+                case "-":
+                    cal.CalculateEvent += new Formula <double> (cal.GetDifference);
+                    lblDisplayTotal.Text = cal.GetDifference(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula <double> (cal.GetDifference);
+                    break;
+                case "*":
+                    cal.CalculateEvent += new Formula <double>(cal.GetProduct);
+                    lblDisplayTotal.Text = cal.GetProduct(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula <double> (cal.GetProduct);
+                    break;
+                case "/":
+                    cal.CalculateEvent += new Formula <double> (cal.GetQuotient);
+                    lblDisplayTotal.Text = cal.GetQuotient(num1, num2).ToString();
+                    cal.CalculateEvent -= new Formula <double> (cal.GetQuotient);
+                    break;
+
+                default:
+                    lblDisplayTotal.Text = "Error!";
+                    break;
+            }
+        }
+
+    }
+}
